c253ef09b5a198de7e89fb1c711c7054c305a65e
Vic <victortagupa@gmail.com>
2023-11-08 14:24:55 +0000

    Adjust font size

--- resources/app/js/components/alerts/base.jsx
+++ resources/app/js/components/alerts/base.jsx
@@ -5,7 +5,7 @@ export default forwardRef((props, ref) => {
     const clone = cloneElement(<div {...props} />, {
         ...props,
         _class:
-            "text-xs text-white bg-opacity-50 p-2 rounded-lg " + props._class,
+            "text-sm text-white bg-opacity-50 p-3 rounded-lg " + props._class,
     });
     return <Base render={<div />} ref={ref} {...clone.props} />;
 });
