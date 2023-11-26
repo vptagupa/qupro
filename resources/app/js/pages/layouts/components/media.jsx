c5c862471154c2ccbc7579a37f50cfeba814f9dd
Vic <victortagupa@gmail.com>
2023-11-26 06:56:33 +0000

    Fix auto play when screen changes

--- resources/app/js/pages/layouts/components/media.jsx
+++ resources/app/js/pages/layouts/components/media.jsx
@@ -1,89 +1,66 @@
 import Image from "./image";
 import Video from "./video";
-import { Transition } from "@headlessui/react";
-import {
-    memo,
-    useCallback,
-    useState,
-    useEffect,
-    useRef,
-    useDeferredValue,
-} from "react";
+import { memo, useCallback, useState, useEffect, useRef } from "react";
 
 export default memo(({ media, interval }) => {
     const ref = useRef(null);
     const [index, setIndex] = useState(0);
-    const deferredIndex = useDeferredValue(index);
+    const [play, setPlay] = useState(media[index] ?? media[0]);
 
     const onEnded = useCallback(
         (index) => {
-            const idx = media.length - 1 > index ? index + 1 : 0;
-
-            setIndex(idx);
-
-            if (media.length > 0) {
-                onPlay();
+            setIndex(media.length - 1 > index ? index + 1 : 0);
+            // Auto play video where there is only 1 video in the media
+            if (media.length == 1 && play.is_video) {
+                onPlayVideo();
             }
         },
-        [media],
+        [media, play],
     );
 
-    const onEndedVideo = () => {
+    const onPlayVideo = () => {
         ref.current.play();
     };
 
     let timeoutId;
-    const onEndedImage = () => {
+    const onImage = () => {
         timeoutId = setTimeout(() => {
-            setIndex(media.length - 1 > index ? index + 1 : 0);
+            onEnded(index);
+            console.log(index);
         }, interval * 1000);
     };
 
     const onPlay = () => {
-        if (media[index].is_video) {
-            onEndedVideo();
+        if (play.is_video) {
+            onPlayVideo();
         }
 
-        if (media[index].is_image) {
-            onEndedImage();
+        if (play.is_image) {
+            onImage();
         }
     };
 
     useEffect(() => {
-        onPlay();
-
+        setPlay(media[index] ?? media[0]);
         return () => clearTimeout(timeoutId);
     }, [index, media]);
+
+    useEffect(() => {
+        onPlay();
+    }, [play]);
 
     return (
         <>
             <div>
                 <div className="flex items-center justify-center p-2">
-                    <Transition
-                        show={deferredIndex == index}
-                        enter="transition-opacity duration-75"
-                        enterFrom="opacity-0"
-                        enterTo="opacity-100"
-                        leave="transition-opacity duration-150"
-                        leaveFrom="opacity-100"
-                        leaveTo="opacity-0"
-                        className="w-full"
-                    >
-                        {media[index].is_image && (
-                            <Image
-                                ref={ref}
-                                url={media[index].file.url}
-                                onEnded={onEnded}
-                            />
-                        )}
-                        {media[index].is_video && (
-                            <Video
-                                ref={ref}
-                                url={media[index].file.url}
-                                onEnded={(e) => onEnded(index)}
-                            />
-                        )}
-                    </Transition>
+                    {play.is_image && <Image url={play.file.url} />}
+                    {play.is_video && (
+                        <Video
+                            ref={ref}
+                            url={play.file.url}
+                            onEnded={(e) => onEnded(index)}
+                        />
+                    )}
                 </div>
             </div>
         </>
