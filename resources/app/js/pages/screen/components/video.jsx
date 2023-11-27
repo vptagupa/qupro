3fce7a6291e44f949eb675958bf1ad61003085f4
Vic <victortagupa@gmail.com>
2023-11-27 12:17:00 +0000

    Add video opacity to blend video in bg

--- resources/app/js/pages/screen/components/video.jsx
+++ resources/app/js/pages/screen/components/video.jsx
@@ -12,7 +12,7 @@ export default forwardRef((props, ref) => {
                     onEnded={(e) => props.onEnded()}
                     src={props.url}
                     width="100%"
-                    className="!w-[100%] rounded-lg"
+                    className="!w-[100%] rounded-lg opacity-75"
                 >
                     Your browser does not support the video tag.
                 </video>
