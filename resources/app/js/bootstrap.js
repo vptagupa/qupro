b97a6662c417e4d30d1087814d545362f819d2f5
Vic <victortagupa@gmail.com>
2023-12-10 06:52:38 +0000

    Replace pusher with websocket

--- resources/app/js/bootstrap.js
+++ resources/app/js/bootstrap.js
@@ -19,14 +19,12 @@ import Echo from "laravel-echo";
 
 import Pusher from "pusher-js";
 window.Pusher = Pusher;
-
+console.log(window.location.hostname);
 window.Echo = new Echo({
     broadcaster: "pusher",
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER ?? "mt1",
-    wsHost: import.meta.env.VITE_PUSHER_HOST
-        ? import.meta.env.VITE_PUSHER_HOST
-        : `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+    wsHost: window.location.hostname,
     wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
     wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
