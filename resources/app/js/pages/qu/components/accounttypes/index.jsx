bab0506642ace12526fdb0782e4e0e42e3482017
Vic <victortagupa@gmail.com>
2023-12-11 07:44:58 +0000

    Auto update stats

--- resources/app/js/pages/qu/components/accounttypes/index.jsx
+++ resources/app/js/pages/qu/components/accounttypes/index.jsx
@@ -10,6 +10,23 @@ const Component = memo(({ controls: { form } }) => {
 
     const [accountTypes, setAccountTypes] = useState(data);
 
+    useEffect(() => {
+        axios
+            .post(route("admin.account-types.list"), {
+                per_page: 100,
+            })
+            .then(({ data: { data } }) => {
+                setAccountTypes(
+                    accountTypes.map((type) => {
+                        const t = data.filter((d) => d.id == type.id)[0];
+                        type.statistics = t.statistics;
+
+                        return type;
+                    }),
+                );
+            });
+    }, []);
+
     return (
         <>
             <div className="w-full min-h-[250px] flex gap-2 justify-center">
